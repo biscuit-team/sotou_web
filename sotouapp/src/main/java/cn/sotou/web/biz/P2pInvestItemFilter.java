38feebb0e83218ca37ebc257e23e26d3d42f48fa
gongshw <gongshw1992@gmail.com>
2014-01-13 15:47:19 +0800

    change to spring mvc

---
+++ sotouapp/src/main/java/cn/sotou/web/biz/P2pInvestItemFilter.java
@@ -0,0 +1,5 @@
+package cn.sotou.web.biz;
+
+public interface P2pInvestItemFilter {
+	
+}
---
+++ sotouapp/src/main/java/cn/sotou/web/biz/impl/P2pInvestItemFilterImpl.java
@@ -0,0 +1,10 @@
+package cn.sotou.web.biz.impl;
+
+import org.springframework.stereotype.Service;
+
+import cn.sotou.web.biz.P2pInvestItemFilter;
+
+@Service
+public class P2pInvestItemFilterImpl implements P2pInvestItemFilter {
+
+}
