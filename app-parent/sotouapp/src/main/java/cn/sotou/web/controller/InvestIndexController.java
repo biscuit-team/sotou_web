f552f9223c541fb34de8d0ef1b1bfff3c4d92099
gongshw <gongshw1992@gmail.com>
2014-01-23 17:31:03 +0800

    add invest detail page.

--- app-parent/sotouapp/src/main/java/cn/sotou/web/controller/InvestIndexController.java
+++ app-parent/sotouapp/src/main/java/cn/sotou/web/controller/InvestIndexController.java
@@ -1,6 +1,7 @@
 package cn.sotou.web.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -11,9 +12,14 @@ public class InvestIndexController {
 	public String index() {
 		return "invest/index";
 	}
-	
+
 	@RequestMapping("/filter")
 	public String filter() {
 		return "invest/filter";
+	}
+
+	@RequestMapping("/{id}")
+	public String detail(@PathVariable("id") long id) {
+		return "invest/detail";
 	}
 }
