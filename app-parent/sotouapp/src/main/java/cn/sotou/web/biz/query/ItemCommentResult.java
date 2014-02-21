40da9954ea23e8f019ced8d77b18f14b5539da6d
shuhzhang <zsh10>
2014-02-21 11:46:01 +0800

    评论模块：编写数据库读写框架

---
+++ app-parent/sotouapp/src/main/java/cn/sotou/web/biz/query/ItemCommentResult.java
@@ -0,0 +1,12 @@
+package cn.sotou.web.biz.query;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: shuhzhang
+ * Date: 14-2-21
+ * Time: 上午11:15
+ * To change this template use File | Settings | File Templates.
+ */
+public class ItemCommentResult {
+
+}
